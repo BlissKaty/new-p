c3db9658db665e02d018e0c46f76c7656579070d
Bliss Kameka <bliss.kameka@gmail.com>
2023-04-25 13:38:10 +0200

    movie images

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Image from "next/image";
 
-const Movies = ({ title, id, poster_path }) => {
-  return <div>{title}</div>;
+const Movies = ({ title, id, poster_path, release_date }) => {
+  const image_path = "https://image.tmdb.org/t/p/w500";
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <Image src={image_path + poster_path} alt={id} width="100" height="100" />
+    </div>
+  );
 };
 
 export default Movies;
