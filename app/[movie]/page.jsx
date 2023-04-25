4db412a215f8403710b67a3cb521f4aeb29151b4
Bliss Kameka <bliss.kameka@gmail.com>
2023-04-25 13:57:06 +0200

    show clicked image

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 async function Movie({ params }) {
   const { movie } = params;
@@ -7,9 +8,20 @@ async function Movie({ params }) {
     `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}&language=en-US&page=1`
   );
 
+  const image_path = "https://image.tmdb.org/t/p/w500";
+
   const res = await data.json();
 
-  return <div>Movie</div>;
+  return (
+    <div>
+      <Image
+        src={image_path + `${res.poster_path}`}
+        alt={res.id}
+        width="100"
+        height="100"
+      />
+    </div>
+  );
 }
 
 export default Movie;
